6d5cb46eaa48c3a1f7c0fccdaa748fbc3283397b
Nguyen Anh Tuan <nguyenanhtuandl1504@gmail.com>
2026-06-11 15:48:42 +0700

    feat: add lab2 middleware and api security config

--- PRN232.LMS.API/Responses/ApiResponse.cs
+++ PRN232.LMS.API/Responses/ApiResponse.cs
@@ -1,7 +1,21 @@
+using System.Runtime.Serialization;
 using PRN232.LMS.Services.Models.Common;
+using PRN232.LMS.Services.Models.Responses;
 
 namespace PRN232.LMS.API.Responses;
 
+[KnownType(typeof(CourseResponse))]
+[KnownType(typeof(CourseSummaryResponse))]
+[KnownType(typeof(EnrollmentResponse))]
+[KnownType(typeof(EnrollmentSummaryResponse))]
+[KnownType(typeof(SemesterResponse))]
+[KnownType(typeof(SemesterSummaryResponse))]
+[KnownType(typeof(StudentResponse))]
+[KnownType(typeof(StudentSummaryResponse))]
+[KnownType(typeof(SubjectResponse))]
+[KnownType(typeof(SubjectSummaryResponse))]
+[KnownType(typeof(UserResponse))]
+[KnownType(typeof(List<object>))]
 public class ApiResponse<T>
 {
     public bool Success { get; set; }
